ccddf6ad9f62619130a76f133985107327944d3d
migara793 <migarasathsara37@gmail.com>
2025-04-06 11:04:56 -0700

    data ingestion completed

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# chest-cancer-project
+## workflow
+1. update config.yaml
+2. update secret.yaml[optional]
+3. update parms.yaml
+4. update the entity
+5. update the configuration manager in src config
+6. update the components
+7. update the pipeline
+8. update main.py
+9. update the dvc.yaml
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from chest_cancer_project import logger
+from chest_cancer_project.pipeline.stage_01_data_ingestion import STAGE_NAME, DataIngestionTrainingPipeline
+STAGE_NAME="data_ingestion "
 
-logger.info("hello world")
+try:
+    logger.info(f">>>>> stage{STAGE_NAME} started <<<<<")
+    obj=DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage{STAGE_NAME} completed <<<<<\n\nx==========x")
 
+except Exception as e:
+    logger.exception(e) 
+    raise e   
