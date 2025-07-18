9f1f527282a9fb870ac8310172978bd3593798b9
migara793 <migarasathsara37@gmail.com>
2025-07-17 23:20:18 -0700

    new commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from chest_cancer_project import logger
-from chest_cancer_project.pipeline.stage_01_data_ingestion import STAGE_NAME, DataIngestionTrainingPipeline
+from chest_cancer_project.pipeline.stage_01_data_ingestion import  DataIngestionTrainingPipeline
+from chest_cancer_project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline 
 STAGE_NAME="data_ingestion "
+
 
 try:
     logger.info(f">>>>> stage{STAGE_NAME} started <<<<<")
@@ -11,3 +13,16 @@ try:
 except Exception as e:
     logger.exception(e) 
     raise e   
+
+
+
+STAGE_NAME ="Prepare base model"
+
+try:
+    logger.info(f">>>>> stage{STAGE_NAME} started <<<<<")
+    obj=PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nX=========X")
+except Exception as e:
+    logger.exception(e)
+    raise e    
